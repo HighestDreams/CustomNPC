34bf52b31889a93a0b6ecc3edcb9f50a36f9d799
HighestDreams <79803040+HighestDreams@users.noreply.github.com>
2021-12-25 10:01:28 -0800

    Sorting emote ids

--- src/HighestDreams/CustomNPC/NPC.php
+++ src/HighestDreams/CustomNPC/NPC.php
@@ -14,13 +14,32 @@ use pocketmine\utils\{Config, TextFormat as COLOR};
 class NPC extends PluginBase
 {
 
-    public const PREFIX = COLOR::BOLD . COLOR::WHITE . "Custom" . COLOR::RED . "NPC " . COLOR::RESET . COLOR::DARK_RED . "> ";
-    public static $editor = [];
-    public static $teleport = [];
-    public static $timer = [];
-    public static $emotes = ["wave" => "4c8ae710-df2e-47cd-814d-cc7bf21a3d67", "simple_clap" => "9a469a61-c83b-4ba9-b507-bdbe64430582", "over_there" => "ce5c0300-7f03-455d-aaf1-352e4927b54d", "diamonds_to_you" => "86b34976-8f41-475b-a386-385080dc6e83", "the_pickaxe" => "d7519b5a-45ec-4d27-997c-89d402c6b57f", "over_here" => "71721c51-b7d1-46b1-b7ea-eb4c4126c3db", "breakdance" => "1dbaa006-0ec6-42c3-9440-a3bfa0c6fdbe", "chatting" => "59d9e78c-f0bb-4f14-9e9b-7ab4f58ffbf5", "the_hammer" => "7cec98d8-55cc-44fe-b0ae-2672b0b2bd37", "golf_clap" => "434489fd-ed42-4814-961a-df14161d67e0", "disappointed" => "a98ea25e-4e6a-477f-8fc2-9e8a18ab7004", "victory_cheer" => "d0c60245-538e-4ea2-bdd4-33477db5aa89", "foot_stomp" => "13334afa-bd66-4285-b3d9-d974046db479", "the_woodpunch" => "42fde774-37d4-4422-b374-89ff13a6535a", "sad_sigh" => "98a68056-e025-4c0f-a959-d6e330ccb5f5", "the_elytra" => "7393aa53-9145-4e66-b23b-ec86def6c6f2", "giddy" => "738497ce-539f-4e06-9a03-dc528506a468", "hooray" => "c4b5b251-24d3-43eb-9c05-46be246aeefb"];
     public static $settings;
     public static $Instance;
+    public static $timer = [];
+    public static $editor = [];
+    public static $teleport = [];
+    public const PREFIX = COLOR::BOLD . COLOR::WHITE . "Custom" . COLOR::RED . "NPC " . COLOR::RESET . COLOR::DARK_RED . "> ";
+    public static $emotes = [ # Didn't take too much to sort this, code is here (https://3v4l.org/Eh0vj)
+        "wave" => "4c8ae710-df2e-47cd-814d-cc7bf21a3d67",
+        "giddy" => "738497ce-539f-4e06-9a03-dc528506a468",
+        "hooray" => "c4b5b251-24d3-43eb-9c05-46be246aeefb",
+        "chatting" => "59d9e78c-f0bb-4f14-9e9b-7ab4f58ffbf5",
+        "sad_sigh" => "98a68056-e025-4c0f-a959-d6e330ccb5f5",
+        "over_here" => "71721c51-b7d1-46b1-b7ea-eb4c4126c3db",
+        "golf_clap" => "434489fd-ed42-4814-961a-df14161d67e0",
+        "over_there" => "ce5c0300-7f03-455d-aaf1-352e4927b54d",
+        "breakdance" => "1dbaa006-0ec6-42c3-9440-a3bfa0c6fdbe",
+        "the_hammer" => "7cec98d8-55cc-44fe-b0ae-2672b0b2bd37",
+        "foot_stomp" => "13334afa-bd66-4285-b3d9-d974046db479",
+        "the_elytra" => "7393aa53-9145-4e66-b23b-ec86def6c6f2",
+        "simple_clap" => "9a469a61-c83b-4ba9-b507-bdbe64430582",
+        "the_pickaxe" => "d7519b5a-45ec-4d27-997c-89d402c6b57f",
+        "disappointed" => "a98ea25e-4e6a-477f-8fc2-9e8a18ab7004",
+        "victory_cheer" => "d0c60245-538e-4ea2-bdd4-33477db5aa89",
+        "the_woodpunch" => "42fde774-37d4-4422-b374-89ff13a6535a",
+        "diamonds_to_you" => "86b34976-8f41-475b-a386-385080dc6e83"
+    ];
 
     public function onEnable()
     {
@@ -39,6 +58,14 @@ class NPC extends PluginBase
                 @mkdir($path);
             }
         }
+
+
+        $new = [];
+        foreach (self::$emotes as $NAME => $ID) {
+            $length = strlen($NAME . $ID);
+            $new[$length] = $NAME;
+        }
+        print_r($new);
 
         foreach ($this->getResources() as $name => $info) {
             if (preg_match('/.+\.png/i', $name) and !is_file($newPath = "{$this->getDataFolder()}Capes\\$name")) {
@@ -202,16 +229,6 @@ class NPC extends PluginBase
     public function spawnedForFistTime(CustomNPC $npc): bool
     {
         return (is_null($npc->namedtag->getCompoundTag("Commands")) and is_null($npc->namedtag->getCompoundTag("Interactions")));
-    }
-
-    public function isInCoolDown(Player $player, CustomNPC $npc): bool
-    {
-        return isset(self::$timer[$npc->getId()][$player->getName()]);
-    }
-
-    public function addCooldown(Player $player, CustomNPC $npc)
-    {
-        self::$timer[$npc->getId()][$player->getName()] = microtime(true);
     }
 
     public function getNPCCooldown(CustomNPC $npc): float
